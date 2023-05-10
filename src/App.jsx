ad2daab282cad86e0469b1ae08050b8228128d3f
Anass Moussadi <a.moussadi03@gmail.com>
2023-05-10 10:32:56 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button'
+
+function App() {
+  return (
+    <>
+      {/* <Button variant="outlined" size="large">hoi</Button> */}
+    </>
+  );
+}
+
+export default App;
